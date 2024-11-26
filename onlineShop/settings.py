d88023cc6019b456ed13db90bb5f390fdce2956e
riverSpades <riverspades33@gmail.com>
2024-11-26 17:59:31 +0000

    updates

--- onlineShop/settings.py
+++ onlineShop/settings.py
@@ -96,18 +96,18 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 # Cookie settings
-#if os.environ.get('ENV')== "prod":  # Assuming you have ENV set to "prod" for production
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-SESSION_COOKIE_DOMAIN = 'onlineshopcosmetics.netlify.app'  # Replace with your custom domain
-CSRF_COOKIE_DOMAIN = 'onlineshopcosmetics.netlify.app'     # Same as above
-CSRF_COOKIE_SAMESITE = 'None' 
-SESSION_COOKIE_SAMESITE = 'None'
-#else:
-#    CSRF_COOKIE_SECURE = False      # Use False for local development
-#    CSRF_COOKIE_SAMESITE = 'Lax'   # Same as above
-#    SESSION_COOKIE_SECURE = False  # Set to True in production to ensure it's sent over HTTPS
-#    SESSION_COOKIE_SAMESITE = 'Lax'  # Adjust as necessary (Lax or None for cross-site)
+if os.environ.get('ENV')== "prod":  # Assuming you have ENV set to "prod" for production
+    CSRF_COOKIE_SECURE = True
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_DOMAIN = 'onlineshopcosmetics.netlify.app'  # Replace with your custom domain
+    CSRF_COOKIE_DOMAIN = 'onlineshopcosmetics.netlify.app'     # Same as above
+    CSRF_COOKIE_SAMESITE = 'None' 
+    SESSION_COOKIE_SAMESITE = 'None'
+else:
+    CSRF_COOKIE_SECURE = False      # Use False for local development
+    CSRF_COOKIE_SAMESITE = 'Lax'   # Same as above
+    SESSION_COOKIE_SECURE = False  # Set to True in production to ensure it's sent over HTTPS
+    SESSION_COOKIE_SAMESITE = 'Lax'  # Adjust as necessary (Lax or None for cross-site)
 
  # Adjust if cross-site requests are necessary
 SESSION_COOKIE_NAME = 'sessionid'  # Default session cookie name
